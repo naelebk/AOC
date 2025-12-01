2ba4a41ab699284109ed04dd88b0789e92feb122
Naël <nael.embarki@aureatechnology.com>
2025-12-01 08:42:20 +0100

    day1 part1 resolved

--- 2025/day1/part1.rb
+++ 2025/day1/part1.rb
@@ -2,3 +2,20 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+input = Utils.read_lines('day1-sample-input.txt')
+code = 50
+count = 0 
+
+input.each do |x|
+    sens = x[0]
+    number = x[1..-1].to_i
+    if sens == "L"
+        code = (code - number) % 100
+    else
+        code = (code + number) % 100
+    end
+    count += 1 if code == 0
+end
+
+puts count
+
