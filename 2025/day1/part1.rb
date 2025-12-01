a3d1449b5a9e3f55f53b8d877a0e84dfeb0ff55b
Naël <nael.embarki@aureatechnology.com>
2025-12-01 09:38:37 +0100

    day1 part 1+2 done

--- 2025/day1/part1.rb
+++ 2025/day1/part1.rb
@@ -7,14 +7,14 @@ code = 50
 count = 0 
 
 input.each do |x|
-    sens = x[0]
-    number = x[1..-1].to_i
-    if sens == "L"
-        code = (code - number) % 100
-    else
-        code = (code + number) % 100
-    end
-    count += 1 if code == 0
+  sens = x[0]
+  number = x[1..-1].to_i
+  if sens == "L"
+    code = (code - number) % 100
+  else
+    code = (code + number) % 100
+  end
+  count += 1 if code == 0
 end
 
 puts count
--- 2025/day1/part2.rb
+++ 2025/day1/part2.rb
@@ -2,3 +2,37 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+input = Utils.read_lines('day1-input.txt')
+code = 50
+count = 0 
+
+input.each do |x|
+  sens = x[0]
+  number = x[1..-1].to_i
+  start = code
+
+  if sens == "L"
+    if start == 0
+      count += number / 100
+    else
+      if number >= start
+        count += 1 + (number - start) / 100
+      end
+    end
+    nouveau = (code - number) % 100
+    code = nouveau
+  else
+    if start == 0
+      count += number / 100
+    else
+      d = (100 - start) % 100
+      if d != 0 && number >= d
+        count += 1 + (number - d) / 100
+      end
+    end
+    nouveau = (code + number) % 100
+    code = nouveau
+  end
+end
+
+puts count
