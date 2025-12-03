fb8f45d9d92c59cfc3e86d8b622e62e51e8ddc31
Naël <nael.embarki@aureatechnology.com>
2025-12-03 06:11:23 +0100

    Day3 done

--- 2025/day03/part1.rb
+++ 2025/day03/part1.rb
@@ -10,5 +10,17 @@ input = Utils.read_lines('day3-input.txt')
 
 sum = 0
 
+input.each do |line|
+  digits = line.strip.chars.map(&:to_i)
+  max_shock = 0
+  (0...digits.length).each do |i|
+    ((i+1)...digits.length).each do |j|
+      shock = digits[i] * 10 + digits[j]
+      max_shock = [max_shock, shock].max
+    end
+  end  
+  sum += max_shock
+end
+
 cookie = Utils.get_cookie
 Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
--- 2025/day03/part2.rb
+++ 2025/day03/part2.rb
@@ -10,5 +10,23 @@ input = Utils.read_lines('day3-input.txt')
 
 sum = 0
 
+input.each do |line|
+  digits = line.strip.chars
+  n = digits.length
+  k = 12
+  result = []
+  start_pos = 0  
+  k.times do |pos|
+    remaining = k - pos
+    max_search_pos = n - remaining
+    max_digit = digits[start_pos..max_search_pos].max
+    idx = digits[start_pos..max_search_pos].index(max_digit)
+    result << max_digit
+    start_pos += idx + 1
+  end
+  joltage = result.join.to_i
+  sum += joltage
+end
+
 cookie = Utils.get_cookie
 Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
