fbaaf32ab6e937efa2d99d43b63a173251bd2689
Naël <nael.embarki@aureatechnology.com>
2025-12-04 08:51:16 +0100

    2023 day2 done

--- 2023/day02/part1.rb
+++ 2023/day02/part1.rb
@@ -2,14 +2,52 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+def is_valid_game(game, max)
+  informations = game.split(':')
+  game_id = informations[0].match(/\d+/)[0].to_i
+  draws = informations[1].split(';').map(&:strip)
+  colors = ["red", "green", "blue"]
+  draws.each do |draw|
+    current_information = {
+      "red" => 0,
+      "green" => 0,
+      "blue" => 0
+    }
+    cubes = draw.split(',').map(&:strip)
+    cubes.each do |cube|
+      parts = cube.split(' ')
+      number = parts[0].to_i
+      color = parts[1]      
+      current_information[color] += number
+    end
+    colors.each do |color|
+      return 0 if current_information[color] > max[color]
+    end
+  end
+  game_id
+end
+
 Utils.time {
   YEAR = 2023
   DAY = 2
   LEVEL = 1
 
-  input = Utils.read_lines('day2-input.txt')
+  max = {
+   "red" => 12,
+   "green" => 13,
+   "blue" => 14
+  }
 
+  input = Utils.read_lines('day2-input.txt')
   sum = 0
+  input.each do |game|
+    sum += is_valid_game(game, max)
+  end
+
+  puts sum
+
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.985958706 secondes
--- 2023/day02/part2.rb
+++ 2023/day02/part2.rb
@@ -2,14 +2,47 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+def product(game)
+  informations = game.split(':')
+  game_id = informations[0].match(/\d+/)[0].to_i
+  draws = informations[1].split(';').map(&:strip)
+  colors = ["red", "green", "blue"]
+  current_information = {
+    "red" => 0,
+    "green" => 0,
+    "blue" => 0
+  }
+  draws.each do |draw|
+    cubes = draw.split(',').map(&:strip)
+    cubes.each do |cube|
+      parts = cube.split(' ')
+      number = parts[0].to_i
+      color = parts[1]  
+      current_information[color] = number if number > current_information[color]
+    end
+  end
+  product = 1
+  colors.each do |color|
+    product *= current_information[color]
+  end
+  product
+end
+
 Utils.time {
   YEAR = 2023
   DAY = 2
   LEVEL = 2
 
   input = Utils.read_lines('day2-input.txt')
-
   sum = 0
+  input.each do |game|
+    sum += product(game)
+  end
+
+  puts sum
+
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.952950605 secondes
