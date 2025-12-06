18492973d024117b3ac4ea060eed94838f4091de
Naël <nael.embarki@aureatechnology.com>
2025-12-06 07:26:04 +0100

    Day 6 done

--- 2025/day06/part1.rb
+++ 2025/day06/part1.rb
@@ -2,14 +2,38 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+def read_columns(path)
+  Utils.read_lines(path).map do |line|
+    line.split.map do |word|
+      if word.length != 1 || word.chars.all? { |c| c.match?(/\d/) }
+        word.to_i
+      else
+        word
+      end
+    end
+  end.transpose
+end
+
 Utils.time {
   YEAR = 2025
   DAY = 6
   LEVEL = 1
 
-  input = Utils.read_lines('day6-input.txt')
-
   sum = 0
+  columns = read_columns("day6-input.txt")
+  columns.each do |column|
+    symbol = column.pop
+    if symbol == "+"
+      sum += column.sum
+    elsif symbol == "*"
+      sum += Utils.product(column)
+    end
+  end
+
+  puts sum
+
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.424442419 secondes
--- 2025/day06/part2.rb
+++ 2025/day06/part2.rb
@@ -2,14 +2,69 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+# Putain j'ai bien galéré sur celui-là ! Le piège c'est qu'il faut lire les nombres
+# de DROITE À GAUCHE dans chaque problème, pas de gauche à droite comme d'habitude
+# J'ai dû reconstruire tout le parsing caractère par caractère pour bien gérer ça
+
+def read_columns(path)
+  Utils.read_lines(path).map do |line|
+    line.split.map do |word|
+      if word.length != 1 || word.chars.all? { |c| c.match?(/\d/) }
+        Utils.digits(word)
+      else
+        word
+      end
+    end
+  end.transpose
+end
+
 Utils.time {
   YEAR = 2025
   DAY = 6
   LEVEL = 2
 
-  input = Utils.read_lines('day6-input.txt')
-
+  lines = Utils.read_lines("day6-input.txt")
   sum = 0
+
+  rows = lines.map { |line| line.chars }
+  max_width = rows.map(&:length).max
+  columns = (0...max_width).map do |col_idx|
+    rows.map { |row| row[col_idx] || ' ' }
+  end
+  problems = []
+  current_problem = []
+  
+  columns.each do |column|
+    if column.all? { |c| c == ' ' }
+      if !current_problem.empty?
+        problems << current_problem
+        current_problem = []
+      end
+    else
+      current_problem << column
+    end
+  end
+  problems << current_problem if !current_problem.empty?
+  
+  problems.each do |problem|
+    symbol = problem[0][-1]
+    numbers = []
+    problem.reverse.each do |column|
+      digits = column[0...-1].join.strip
+      numbers << digits.to_i if !digits.empty? && digits.match?(/^\d+$/)
+    end    
+    result = if symbol == '+'
+      numbers.sum
+    else
+      Utils.product(numbers)
+    end    
+    sum += result
+  end
+  
+  puts sum
+  
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.464816436 secondes
