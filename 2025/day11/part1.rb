ba3700ad6e73960c311297a5d3120ff5cc9d6b83
Naël <nael.embarki@aureatechnology.com>
2025-12-11 06:36:08 +0100

    Day 11 done

--- 2025/day11/part1.rb
+++ 2025/day11/part1.rb
@@ -2,14 +2,35 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+
+def count_paths(graph, start, target, cache = {})
+  return cache[start] if cache.key?(start)
+  return 1 if start == target
+  return 0 unless graph.key?(start)
+  total = graph[start].sum { |neighbor| count_paths(graph, neighbor, target, cache) }
+  cache[start] = total
+  total
+end
+
 Utils.time {
   YEAR = 2025
   DAY = 11
   LEVEL = 1
-
+  
   input = Utils.read_lines('day11-input.txt')
+  graph = Utils.empty_graph
+  input.each do |line|
+    parts = line.split(':')
+    from = parts[0].strip
+    dest = parts[1].strip.split
+    dest.each { |to| graph[from] << to }
+  end
+  sum = count_paths(graph, "you", "out")
 
-  sum = 0
+  puts sum
+
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.480583528 secondes
--- 2025/day11/part2.rb
+++ 2025/day11/part2.rb
@@ -2,14 +2,50 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+def count_paths_with_required(graph, start, target, required)
+  # mask = 0b00 = aucun, 0b01 = dac, 0b10 = fft, 0b11 = les deux
+  # c'est plus optimisé car grâce au bitmasking 
+  # le Set ne devient plus qu'un simple entier !
+  required_list = required.to_a
+  cache = {}  
+  recurse = lambda do |node, mask|
+    return 1 if node == target && mask == (1 << required.size) - 1
+    return 0 if node == target
+    return 0 unless graph.key?(node)
+    key = [node, mask]
+    return cache[key] if cache.key?(key)
+    new_mask = mask
+    required_list.each_with_index do |request, index|
+      new_mask |= (1 << index) if node == request
+    end
+    result = graph[node].sum { |neighbor| recurse.call(neighbor, new_mask) }
+    cache[key] = result
+    result
+  end
+  recurse.call(start, 0)
+end
+
 Utils.time {
   YEAR = 2025
   DAY = 11
   LEVEL = 2
-
+  
   input = Utils.read_lines('day11-input.txt')
+  graph = Utils.empty_graph
+  input.each do |line|
+    parts = line.split(':')
+    from = parts[0].strip
+    dest = parts[1].strip.split
+    dest.each { |to| graph[from] << to }
+  end
+  good = Set.new(["dac", "fft"]) # les chemin doivent passer par ça
+  sum = count_paths_with_required(graph, "svr", "out", good)
+  
+  puts sum
 
-  sum = 0
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+
+# Execution: 0.440647801 secondes
+
