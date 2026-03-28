8db18946fd104171d3da89e885710fb21d8fa476
Naël <nael.embarki@aureatechnology.com>
2026-03-29 00:44:23 +0100

    2023 day13 done

--- 2023/day13/part1.rb
+++ 2023/day13/part1.rb
@@ -2,14 +2,32 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+def find_reflection(matrix)
+  (0...matrix.length - 1).each do |i|
+    if matrix[i] == matrix[i + 1]
+      size = [i + 1, matrix.length - (i + 1)].min
+      if (0...size).all? { |offset| matrix[i - offset] == matrix[i + 1 + offset] }
+        return i + 1
+      end
+    end
+  end
+  0
+end
+
 Utils.time {
   YEAR = 2023
   DAY = 13
   LEVEL = 1
 
-  input = Utils.read_lines('day13-input.txt')
+  sum = Utils.read_blocks('day13-input.txt').sum do |block|
+    horizontal = find_reflection(block)
+    vertical = find_reflection(block.map(&:chars).transpose.map(&:join))
+    horizontal * 100 + vertical
+  end
+  
+  puts sum
 
-  sum = 0
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+# Execution: 0.435189035 secondes
--- 2023/day13/part2.rb
+++ 2023/day13/part2.rb
@@ -2,14 +2,37 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+def count_diffs(s1, s2)
+  (0...s1.length).count { |i| s1[i] != s2[i] }
+end
+
+def find_reflection(matrix)
+  (0...matrix.length - 1).each do |i|
+    total_diffs = 0
+    size = [i + 1, matrix.length - (i + 1)].min
+    (0...size).each do |offset|
+      total_diffs += count_diffs(matrix[i - offset], matrix[i + 1 + offset])
+      break if total_diffs > 1
+    end
+    return i + 1 if total_diffs == 1
+  end
+  0
+end
+
 Utils.time {
   YEAR = 2023
   DAY = 13
   LEVEL = 2
 
-  input = Utils.read_lines('day13-input.txt')
+  sum = Utils.read_blocks('day13-input.txt').sum do |block|
+    horizontal = find_reflection(block)
+    vertical = find_reflection(block.map(&:chars).transpose.map(&:join))
+    horizontal * 100 + vertical
+  end
+  
+  puts sum
 
-  sum = 0
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+# Execution: 0.420183136 secondes
