872bddd4fe08bca63ecef9f5c55539bf7fae8993
Naël <nael.embarki@aureatechnology.com>
2026-08-05 16:33:32 +0200

    AOC 2024 day4 done

--- 2024/day04/part1.rb
+++ 2024/day04/part1.rb
@@ -2,14 +2,41 @@
 # part1.rb
 require_relative '../../utils.rb'
 
+WORD = "XMAS"
+DIRECTIONS = Utils.neighbors8(0,0)
+
+def find_xmas_in_matrix(matrix)
+  height, width = Utils.get_size_of_grid(matrix)
+  word_len = WORD.length
+  count = 0
+  (0...height).each do |row|
+    (0...width).each do |col|
+      next unless matrix[row][col] == WORD[0]
+      DIRECTIONS.each do |d_row, d_col|
+        end_r = row + d_row * (word_len - 1)
+        end_c = col + d_col * (word_len - 1)
+        next unless end_r.between?(0, height - 1) && end_c.between?(0, width - 1)
+        match = (1...word_len).all? do |i|
+          matrix[row + d_row * i][col + d_col * i] == WORD[i]
+        end
+        count += 1 if match
+      end
+    end
+  end
+  count
+end
+
 Utils.time {
   YEAR = 2024
   DAY = 04
   LEVEL = 1
 
-  input = Utils.read_lines('day04-input.txt')
-  sum = 0
+  matrix = Utils.read_matrix('day04-input.txt')
+  sum = find_xmas_in_matrix(matrix)
+
+  puts sum
 
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+# Execution: 0.629426139 secondes
--- 2024/day04/part2.rb
+++ 2024/day04/part2.rb
@@ -2,14 +2,35 @@
 # part2.rb
 require_relative '../../utils.rb'
 
+WORD = "XMAS"
+DIRECTIONS = Utils.neighbors8(0,0)
+
+def find_xmas_in_matrix(matrix)
+  height, width = Utils.get_size_of_grid(matrix)
+  count = 0
+  # on évite les bords car le 'A' doit être au centre d'une croix 3x3
+  (1...(height - 1)).each do |row|
+    (1...(width - 1)).each do |col|
+      next unless matrix[row][col] == 'A'
+      diag1 = matrix[row - 1][col - 1] + matrix[row + 1][col + 1]
+      diag2 = matrix[row - 1][col + 1] + matrix[row + 1][col - 1]
+      count += 1 if (diag1 == "MS" || diag1 == "SM") && (diag2 == "MS" || diag2 == "SM")
+    end
+  end
+  count
+end
+
 Utils.time {
   YEAR = 2024
   DAY = 04
   LEVEL = 2
 
-  input = Utils.read_lines('day04-input.txt')
-  sum = 0
+  input = Utils.read_matrix('day04-input.txt')
+  sum = find_xmas_in_matrix(input)
+
+  puts sum
 
   cookie = Utils.get_cookie
   Utils.submit_answer(YEAR, DAY, LEVEL, sum, cookie)
 }
+# Execution: 0.695961352 secondes
